e2c65b53957adeac571900352de14f818248dba4
Dale <djblack@uci.edu>
2021-08-15 21:41:04 -0700

    add extra `mask_elements` test

--- test/utils.jl
+++ test/utils.jl
@@ -19,5 +19,23 @@
         @test mask_elements(img, 0, 2) == answer
     end
 
-    @testset ExtendedTestSet "mask_elements" begin end
+    @testset ExtendedTestSet "mask_elements" begin
+        img = [
+            1 -2 -2 -2
+            -2 -2 -2 -2
+            1 -2 -2 -2
+            -2 -2 -2 -2
+            1 -2 -2 -2
+            -2 -2 -2 -2
+        ]
+        answer = Bool.([
+            1 0 0 0
+            0 0 0 0
+            1 0 0 0
+            0 0 0 0
+            1 0 0 0
+            0 0 0 0
+        ])
+        @test mask_elements(img, 0, 2) == answer
+    end
 end
