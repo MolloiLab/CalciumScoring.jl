3d0ad1537d9ec63faddabff92c61db9642a5629c
Dale-Black <djblack@uci.edu>
2022-04-19 16:29:16 -0700

    fix SpatiallyWeighted 3D

--- src/spatially_weighted.jl
+++ src/spatially_weighted.jl
@@ -1,34 +1,41 @@
-abstract type CalciumScore end
+# abstract type CalciumScore end
 struct SpatiallyWeighted <: CalciumScore end
 
-function score(vol, calibration, alg::SpatiallyWeighted)
+function score(vol::AbstractMatrix, calibration, alg::SpatiallyWeighted)
+    μ, σ= mean(calibration), std(calibration)
+	d = Distributions.Normal(μ, σ)
+
+	scaled_array = zeros(size(vol))
+	for i in 1:size(vol, 1)
+		for j in 1:size(vol, 2)
+			scaled_array[i, j] = Distributions.cdf(d, vol[i, j])
+		end
+	end
+
+	kern = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
+	weighted_arr = DSP.conv(scaled_array, kern)[2:end-1, 2:end-1]
+    
+	return sum(weighted_arr)
+end
+
+function score(vol::AbstractArray, calibration, alg::SpatiallyWeighted)
     μ, σ = mean(calibration), std(calibration)
 	d = Distributions.Normal(μ, σ)
 
 	scaled_array = zeros(size(vol))
 	for i in 1:size(vol, 1)
 		for j in 1:size(vol, 2)
-            if length(size(vol)) == 3
-                for z in 1:size(vol, 3)
-                    scaled_array[i, j, z] = Distributions.cdf(d, vol[i, j, z])
-                end
-            else
-			    scaled_array[i, j] = Distributions.cdf(d, vol[i, j])
-            end
+			for z in 1:size(vol, 3)
+				scaled_array[i, j, z] = Distributions.cdf(d, vol[i, j, z])
+			end
 		end
 	end
 
-    local weighted_arr
-    if length(size(vol)) == 3
-		kern1 = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
-		kern2 = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
-		kern3 = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
-		kern = cat(kern1, kern2, kern3, dims=3)
-        weighted_arr = DSP.conv(scaled_array, kern)[2:end-1, 2:end-1, 2:end-1]
-    else
+	weighted_arr = zeros(size(vol))
+	for z in 1:size(scaled_array, 3)
 		kern = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
-	    weighted_arr = DSP.conv(scaled_array, kern)[2:end-1, 2:end-1]
-    end
+		weighted_arr[:, :, z] = DSP.conv(scaled_array[:, :, z], kern)[2:end-1, 2:end-1]
+	end
     
 	return sum(weighted_arr)
 end
