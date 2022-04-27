3f978cf984a6e8ea51f937c3d0449068ad1c30c9
Dale-Black <djblack@uci.edu>
2022-04-26 17:38:26 -0700

    update swcs to allow manual inputs
    mean and std

--- src/spatially_weighted.jl
+++ src/spatially_weighted.jl
@@ -18,8 +18,45 @@ function score(vol::AbstractMatrix, calibration, alg::SpatiallyWeighted)
 	return sum(weighted_arr)
 end
 
+function score(vol::AbstractMatrix, μ, σ, alg::SpatiallyWeighted)
+	d = Distributions.Normal(μ, σ)
+
+	scaled_array = zeros(size(vol))
+	for i in 1:size(vol, 1)
+		for j in 1:size(vol, 2)
+			scaled_array[i, j] = Distributions.cdf(d, vol[i, j])
+		end
+	end
+
+	kern = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
+	weighted_arr = DSP.conv(scaled_array, kern)[2:end-1, 2:end-1]
+    
+	return sum(weighted_arr)
+end
+
 function score(vol::AbstractArray, calibration, alg::SpatiallyWeighted)
     μ, σ = mean(calibration), std(calibration)
+	d = Distributions.Normal(μ, σ)
+
+	scaled_array = zeros(size(vol))
+	for i in 1:size(vol, 1)
+		for j in 1:size(vol, 2)
+			for z in 1:size(vol, 3)
+				scaled_array[i, j, z] = Distributions.cdf(d, vol[i, j, z])
+			end
+		end
+	end
+
+	weighted_arr = zeros(size(vol))
+	for z in 1:size(scaled_array, 3)
+		kern = [0 0.2 0; 0.2 0.2 0.2; 0 0.2 0]
+		weighted_arr[:, :, z] = DSP.conv(scaled_array[:, :, z], kern)[2:end-1, 2:end-1]
+	end
+    
+	return sum(weighted_arr)
+end
+
+function score(vol::AbstractArray, μ, σ, alg::SpatiallyWeighted)
 	d = Distributions.Normal(μ, σ)
 
 	scaled_array = zeros(size(vol))
