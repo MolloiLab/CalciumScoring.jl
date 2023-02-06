2b48a1e35d6415fc3dad91a0fdf132c2b5fb9b1a
Dale-Black <djblack@uci.edu>
2023-02-06 14:35:22 -0800

    🔨 fix broken agatston test

--- test/agatston.jl
+++ test/agatston.jl
@@ -7,34 +7,34 @@ using InteractiveUtils
 # ╔═╡ f914d5dc-9d93-49df-b05d-2a752ef8bc60
 # ╠═╡ show_logs = false
 begin
-	using Pkg
-	Pkg.activate("..")
-	using Revise
-	using PlutoUI
-	using Test
-	using CalciumScoring
+    using Pkg
+    Pkg.activate("..")
+    using Revise
+    using PlutoUI
+    using Test
+    using CalciumScoring
 end
 
 # ╔═╡ 6788b122-50b1-11ed-3603-c30ea99a0af2
 @testset "Agatston" begin
-	v1 = ones((4, 2, 2))
-	v2 = zeros((4, 2, 2))
-	vol = hcat(v1, v2) * 400
-	spacing = [0.5, 0.5]
-	alg = Agatston()
-	answer = 16
-	@test score(vol, spacing, alg) == answer
+    v1 = ones((4, 2, 2))
+    v2 = zeros((4, 2, 2))
+    vol = hcat(v1, v2) * 400
+    spacing = [0.5, 0.5]
+    alg = Agatston()
+    answer = 16
+    @test score(vol, spacing, alg) == answer
 end
 
 @testset "Agatston Mass" begin
-	v1 = ones((4, 2, 2))
-	v2 = zeros((4, 2, 2))
-	vol = hcat(v1, v2) * 400
-	spacing = [0.5, 0.5, 0.5]
-	alg = Agatston()
-	mass_cal_factor = 0.00075
-	agat_score, mass_score = score(vol, spacing, mass_cal_factor, alg)
-	@test ≈  16 && mass_score ≈ 0.6
+    v1 = ones((4, 2, 2))
+    v2 = zeros((4, 2, 2))
+    vol = hcat(v1, v2) * 400
+    spacing = [0.5, 0.5, 0.5]
+    alg = Agatston()
+    mass_cal_factor = 0.00075
+    agat_score, mass_score = score(vol, spacing, mass_cal_factor, alg)
+    @test agat_score ≈ 16 && mass_score ≈ 0.6
 end
 
 # ╔═╡ Cell order:
