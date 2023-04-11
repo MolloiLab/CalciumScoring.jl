6cc303a0d65bc93ab64dd996d4332af8915dbbc9
Dale-Black <djblack@uci.edu>
2023-04-11 13:14:42 -0700

    add unitful capabalities via multiple dispatch

--- src/agatston.jl
+++ src/agatston.jl
@@ -10,6 +10,7 @@ begin
     using Pkg
     Pkg.activate("..")
     using Revise, PlutoUI, ImageMorphology, CalciumScoring, Statistics
+	using Unitful
     using Unitful: mm, mg, ustrip
     using ImageMorphology: label_components
 end
@@ -190,6 +191,60 @@ function score(vol, spacing, alg::Agatston; kV=120, min_size=1)
     return agatston_score, volume_score
 end
 
+# ╔═╡ 661f1cc0-0095-4616-845c-146d7294bd60
+"""
+```julia
+score(vol, spacing, alg::Agatston; kV=120, min_size_mm=1)
+```
+
+Given an input `vol` and known pixel/voxel `spacing`, calculate the calcium score via the traditional Agatston scoring technique, as outlined in the [original paper](10.1016/0735-1097(90)90282-T). Energy (`kV`) specific `threshold`s are determined based on previous [publications](https://doi.org/10.1093/ehjci/jey019). Also, it converts the Agatston score to a calcium mass score via the `mass_cal_factor`
+
+#### Inputs
+- `vol`: input volume containing just the region of interest
+- `spacing`: known pixel/voxel spacing
+- `alg::Agatston`: Agatston scoring algorithm `Agatston()`
+- kwargs:
+  - `kV=120`: energy of the input CT scan image
+  - `min_size=1`: minimum connected component size (see [`label_components`](https://github.com/JuliaImages/Images.jl))
+
+#### Returns
+- `agatston_score`: total Agatston score
+- `volume_score`: total calcium volume via Agatston scoring
+"""
+function score(vol, spacing::Array{T}, alg::Agatston; kV=120, min_size=1) where T <: Unitful.Quantity
+    @assert (kV == 70 || kV == 80 || kV == 100 || kV == 120 || kV == 135) "kV is $(kV), which is not one of the accepted values \n kV can be 70, 80, 100, 120 or 135"
+    spacing = ustrip(spacing)
+    threshold = round(378 * exp(-0.009 * kV))
+    area = spacing[1] * spacing[2]
+    min_size_pixels = div(round(min_size / area), 1)
+    comp_connect = Int(min(3, max(1, div(round(2 * div(min_size_pixels, 2) + 1), 1))))
+    agatston_score = 0
+    volume_score = 0mm^3
+    for z in axes(vol, 3)
+        slice = vol[:, :, z]
+        thresholded_slice = slice .> threshold
+        max_intensity = maximum(slice)
+        if max_intensity < threshold
+            continue
+        end
+        lesion_map = label_components(thresholded_slice, trues(comp_connect, comp_connect))
+        for label_idx in eachindex(unique(lesion_map))
+            idxs = findall(x -> x == label_idx, lesion_map)
+            num_label_idxs = length(idxs)
+            if num_label_idxs < min_size_pixels
+                continue
+            end
+            intensities = slice[idxs]
+            max_intensity = maximum(intensities)
+            weight = _weight_thresholds(kV, max_intensity)
+            slice_score = num_label_idxs * area * min(weight, 4)
+            agatston_score += slice_score
+            volume_score += num_label_idxs * spacing[1]mm * spacing[2]mm * spacing[3]mm
+        end
+    end
+    return agatston_score, volume_score
+end
+
 # ╔═╡ 728bf5df-c3c7-492e-8735-cb0822e0be20
 md"""
 ## Agatston, Volume, and Mass Score
@@ -260,6 +315,71 @@ function score(vol, spacing, mass_calibration_factor, alg::Agatston; kV=120, min
     return agatston_score, volume_score, abs_mass_score
 end
 
+# ╔═╡ dfc6e26b-5b48-4a05-b5d3-0337e45c243e
+"""
+```julia
+score(vol, spacing, mass_cal_factor, alg::Agatston; kV=120, min_size_mm=1)
+```
+
+Given an input `vol` and known pixel/voxel `spacing`, calculate the calcium score via the traditional Agatston scoring technique, as outlined in the [original paper](10.1016/0735-1097(90)90282-T). Energy (`kV`) specific `threshold`s are determined based on previous [publications](https://doi.org/10.1093/ehjci/jey019). Also, it converts the Agatston score to a calcium mass score via the `mass_cal_factor`
+
+#### Inputs
+- `vol`: input volume containing just the region of interest
+- `spacing`: known pixel/voxel spacing
+- `mass_calibration_factor`: water rod measurement used for converting Agatston score to mass
+- `alg::Agatston`: Agatston scoring algorithm `Agatston()`
+- kwargs:
+  - `kV=120`: energy of the input CT scan image
+  - `min_size=1`: minimum connected component size (see [`label_components`](https://github.com/JuliaImages/Images.jl))
+
+#### Returns
+- `agatston_score`: total Agatston score
+- `volume_score`: total calcium volume via Agatston scoring
+- `mass_score`: total calcium mass via Agatston scoring
+"""
+function score(vol, spacing::Array{T}, mass_calibration_factor::Unitful.Quantity, alg::Agatston; kV=120, min_size=1) where T <: Unitful.Quantity
+    @assert (kV == 70 || kV == 80 || kV == 100 || kV == 120 || kV == 135) "kV is $(kV), which is not one of the accepted values \n kV can be 70, 80, 100, 120 or 135"
+    spacing = ustrip(spacing)
+    threshold = round(378 * exp(-0.009 * kV))
+    area = spacing[1] * spacing[2]
+    min_size_pixels = div(round(min_size / area), 1)
+    comp_connect = Int(min(3, max(1, div(round(2 * div(min_size_pixels, 2) + 1), 1))))
+    agatston_score = 0
+    volume_score = 0mm^3
+    attenuations = []
+    for z in axes(vol, 3)
+        slice = vol[:, :, z]
+        thresholded_slice = slice .> threshold
+        max_intensity = maximum(slice)
+        if max_intensity < threshold
+            continue
+        end
+        lesion_map = label_components(thresholded_slice, trues(comp_connect, comp_connect))
+        for label_idx in eachindex(unique(lesion_map))
+            idxs = findall(x -> x == label_idx, lesion_map)
+            num_label_idxs = length(idxs)
+            if num_label_idxs < min_size_pixels
+                continue
+            end
+            intensities = slice[idxs]
+            max_intensity = maximum(intensities)
+            weight = _weight_thresholds(kV, max_intensity)
+            slice_score = num_label_idxs * area * min(weight, 4)
+            agatston_score += slice_score
+            volume_score += num_label_idxs * spacing[1]mm * spacing[2]mm * spacing[3]mm
+            push!(attenuations, intensities...)
+        end
+    end
+    local rel_mass_score
+    if length(attenuations) == 0
+        rel_mass_score = 0
+    else
+        rel_mass_score = mean(attenuations) * volume_score
+    end
+    abs_mass_score = rel_mass_score * mass_calibration_factor
+    return agatston_score, volume_score, abs_mass_score
+end
+
 # ╔═╡ Cell order:
 # ╠═5169922e-50ae-11ed-2236-2f56e5a20360
 # ╠═c73efe8b-e72b-48a8-a966-5192d9c99abb
@@ -269,5 +389,7 @@ end
 # ╠═ad02e243-bd61-4a72-913e-1cb901a52cc5
 # ╟─f32c3de1-212e-47db-b386-040a5ebc2cd0
 # ╠═53827f4b-b970-48a8-bd29-7488b777ca19
+# ╠═661f1cc0-0095-4616-845c-146d7294bd60
 # ╟─728bf5df-c3c7-492e-8735-cb0822e0be20
 # ╠═ee2a227e-79cf-445a-8c87-45f2738101c2
+# ╠═dfc6e26b-5b48-4a05-b5d3-0337e45c243e
--- test/agatston.jl
+++ test/agatston.jl
@@ -58,8 +58,33 @@ end
 	@test unique(scores) == [12]
 end
 
+# ╔═╡ 5a89e57e-e82f-41f1-ab35-eec72c647ebf
+@testset "Agatston Unitful" begin
+    v1 = ones((4, 2, 2))
+    v2 = zeros((4, 2, 2))
+    vol = hcat(v1, v2) * 400
+    spacing = [0.5, 0.5, 0.5]mm
+    alg = Agatston()
+    agatston_score, volume_score = score(vol, spacing, alg)
+    @test agatston_score ≈ 16 && volume_score == 2mm^3
+end
+
+# ╔═╡ fbc3acff-432a-48f6-b4d2-5be117c420d3
+@testset "Agatston Mass" begin
+    v1 = ones((4, 2, 2))
+    v2 = zeros((4, 2, 2))
+    vol = hcat(v1, v2) * 400
+    spacing = [0.5, 0.5, 0.5]mm
+    alg = Agatston()
+    mass_cal_factor = 0.00075mg / mm^3
+    agatston_score, volume_score, mass_score = score(vol, spacing, mass_cal_factor, alg)
+    @test agatston_score ≈ 16 && mass_score ≈ 0.6mg
+end
+
 # ╔═╡ Cell order:
 # ╠═f914d5dc-9d93-49df-b05d-2a752ef8bc60
 # ╠═01cb8e86-5072-42b6-b262-2412e3192ed4
 # ╠═6788b122-50b1-11ed-3603-c30ea99a0af2
 # ╠═f73e5e28-daa5-4a14-8493-9d5cad0c55ce
+# ╠═5a89e57e-e82f-41f1-ab35-eec72c647ebf
+# ╠═fbc3acff-432a-48f6-b4d2-5be117c420d3
