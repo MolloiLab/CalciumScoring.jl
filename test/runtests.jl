8bfc00d4bbcc97fbe20dee778ef413cab17d20fa
Dale Black <djblack@uci.edu>
2024-06-05 12:03:40 -0700

    (type) stablaize agatston scoring & test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,6 @@
+using Preferences: set_preferences!
+set_preferences!("CalciumScoring", "instability_check" => "error")
+
 include("agatston.jl")
 include("integrated.jl")
 include("spatially_weighted.jl")
